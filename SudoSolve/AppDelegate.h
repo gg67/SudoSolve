8211e30ab4d2542d04b7ac8c8fcadb48575540ac
gg67 <graham.gaylor@gmail.com>
2012-09-27 10:05:18 -0500

    Initial Commit

---
+++ SudoSolve/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SudoSolve
+//
+//  Created by Graham Gaylor on 9/27/12.
+//  Copyright (c) 2012 GGD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SudoSolve/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SudoSolve
+//
+//  Created by Graham Gaylor on 9/27/12.
+//  Copyright (c) 2012 GGD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
